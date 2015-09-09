70f45547513b68f29ac935d032d1b7bb810ab64f
afwillia <af_williams@outlook.com>
2015-09-09 14:51:54 -0700

    line tool measurements
    scripts for measuring pixels within selection and distance to cell
    centers

---
+++ Selection_pixel_location.ijm
@@ -0,0 +1,4 @@
+run("Interpolate", "interval=1"); // Edit>Selection>Interpolate. may be optional
+  getSelectionCoordinates(x, y); 
+for (i=0; i<x.length; i++)
+         print(i+" "+x[i]+" "+y[i]);
---
+++ distance_from_dej.R
@@ -0,0 +1,34 @@
+cells <- read.csv("C://Users/afwillia/Nuclei.csv", stringsAsFactors = FALSE)
+dej <- read.delim("C://Users/afwillia/dej_coords.txt", sep = " ", stringsAsFactors = FALSE, header = FALSE)
+
+#add and subtract 10 units from each dej y-position to account for thickness of DEJ
+
+ymax <- dej[,3] + 10
+ymin <- dej[,3] - 10
+
+dej_adjust <- data.frame(x = rep(dej[,2],2), y = c(ymin, ymax))
+
+#function to find distance between cell and dej pixels
+distance <- function(cx, cy, dx, dy) {
+  (( cx - dx ) ^2 + (cy - dy ) ^2) ^(1/2)
+}
+
+#for each cell in CP file, compare distance to DEJ object, return the smallest distance.
+# postive value if above DEJ, negative if below
+cell_dist <- rep(NA, nrow(cells))
+cell_dist <- sapply(1:nrow(cells), function(i) {
+  
+  #calculate distance from cell center to each pixel on selection line
+  minim <- distance(cells$Location_Center_X[i], cells$Location_Center_Y[i], dej_adjust[,1], dej_adjust[,2])
+  closest_dist <- min(minim)
+  
+  #get nearest x cord to cell, check if cell is within DEJ boundaries, above, or below
+  x_loc <- dej_adjust[which(minim == min(minim)), 1]
+  dej_boundary <- dej_adjust[dej_adjust[,1]==x_loc, 2]
+  
+  if (closest_dist > dej_boundary[2]) closest_dist <- -closest_dist
+  
+  closest_dist
+
+})
+
